67a8343a65bc3874f25b3cff21a67456b5048420
IndicareLeve <indicare.leve@gmail.com>
2022-06-23 14:25:06 +0200

    netlify form

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -4,7 +4,7 @@
 			<span class="font-bold text-3xl md:text-center"
 				>Contattaci per informazioni o un preventivo gratuito!</span
 			>
-			<form class="grid grid-cols-1 gap-6" name="2f-contatti" method="POST" data-netlify="true">
+			<form class="grid grid-cols-1 gap-6" name="2f-contatti" method="POST" netlify>
 				<input
 					type="text"
 					placeholder="Nome"
